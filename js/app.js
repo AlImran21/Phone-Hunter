4bbde0b7fb932770dfa45d1d72c07d7759ce5d44
Al-Imran <alimran21feb@gmail.com>
2022-03-01 17:16:56 +0600

    card added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-console.log('search clicked');
+const searchPhone = () => {
+    const searchField = document.getElementById ('search-field');
+    const searchText = searchField.value;
+    // console.log (searchText);
+
+    searchField.value = '';
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    // console.log (url);
+    fetch(url)
+    .then(res => res.json () )
+    .then(json => displaySearchResult(json.data));
+}
+
+const displaySearchResult = data => {
+    // console.log(data);
+    const searchResult = document.getElementById ('search-result');
+    data.forEach (brand => {
+        console.log (brand);
+        const div = document.createElement ('div');
+        div.classList.add ('col');
+        div.innerHTML = `
+        <div class="card">
+            <img src="${brand.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">${brand.brand}</p>
+              <h5 class="card-title">${brand.phone_name}</h5>
+            </div>
+          </div>
+        `;
+        searchResult.appendChild (div);
+    });
+}
